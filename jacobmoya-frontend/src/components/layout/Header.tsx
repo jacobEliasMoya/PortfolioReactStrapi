e297be472352607c2860066827699feb856e12a1
jacob moya <jemoya1994@Gmail.com>
2024-10-15 20:21:18 -0700

    added a new type/array of objs containing link-menu items

--- jacobmoya-frontend/src/components/layout/Header.tsx
+++ jacobmoya-frontend/src/components/layout/Header.tsx
@@ -4,30 +4,87 @@ import AnchorLink from '../TextHeadingLinks/AnchorLink';
 type Props = {}
 
 export default function Header({}: Props) {
-  return (
-    <header className='flex items-center justify-center p-6 text-orange-300 border-b-4 border-orange-300'>
-        <div className="flex items-center justify-start w-3/12 relative -top-6 ">
-            <h1 className=' text-black bg-orange-300 p-4 w-max font-bold'>
-                JM
-            </h1>
-        </div>
 
-        <nav className="nav-container text-center w-6/12 uppercase ">
-            <ul className='w-full flex items-center justify-between text-3xl '>
-                <ListItem itemtext={'Home'} itemClass={'w-2/12'} itemChildren={<AnchorLink anchorLink={'#'} anchorTitle={'home-link'} anchorText={'Home'} anchorClass={'hover:font-bold '}/>}/>
-                <ListItem itemtext={'Websites'} itemClass={'w-2/12'} itemChildren={<AnchorLink anchorLink={'#'} anchorTitle={'home-link'} anchorText={'Websites'} anchorClass={'hover:font-bold'}/>}/>
-                <ListItem itemtext={'Projects'} itemClass={'w-2/12'} itemChildren={<AnchorLink anchorLink={'#'} anchorTitle={'home-link'} anchorText={'Projects'} anchorClass={'hover:font-bold'}/>}/>
-                <ListItem itemtext={'Code'} itemClass={'w-2/12'} itemChildren={<AnchorLink anchorLink={'#'} anchorTitle={'home-link'} anchorText={'Code'} anchorClass={'hover:font-bold'}/>}/>
-                <ListItem itemtext={'Contact'} itemClass={'w-2/12'} itemChildren={<AnchorLink anchorLink={'#'} anchorTitle={'home-link'} anchorText={'Contact'} anchorClass={'hover:font-bold'}/>}/>
-            </ul>
-        </nav>
+    interface NavItems {
+        text:string,
+        link:string,
+        class:string,
+        title:string
+    }
 
-        <div className="flex items-center justify-end logo-container text-right w-3/12 -top-6 relative">
-            <h1 className=' text-black bg-orange-300 p-4 w-max font-bold'>
-                JM
-            </h1>
-        </div>
+    const navigation:NavItems[] = [
 
-    </header>
-  )
+        {
+            text: 'Home',
+            link: '#',
+            class: 'w-2/12 rounded-lg p-6',
+            title: 'Home'
+        },
+        {
+            text: 'Websites',
+            link: '#',
+            class: 'w-2/12 rounded-lg p-6',
+            title: 'Websites'
+        },
+        {
+            text: 'Projects',
+            link: '#',
+            class: 'w-2/12 rounded-lg p-6',
+            title: 'Projects'
+        },
+        {
+            text: 'Code',
+            link: '#',
+            class: 'w-2/12 rounded-lg p-6',
+            title: 'Code'
+        },
+        {
+            text: 'Contact',
+            link: '#',
+            class: 'w-2/12 rounded-lg p-6',
+            title: 'Contact'
+        },
+
+
+    ]
+
+    return (
+        <header className='flex items-center justify-center p-4 text-orange-300 border-b-4 border-orange-300'>
+            <div className="flex items-center justify-start w-3/12 relative ">
+                <h1 className=' text-black bg-orange-300 p-4 w-max font-bold'>
+                    JM
+                </h1>
+            </div>
+
+            <nav className="nav-container text-center w-6/12 uppercase ">
+                <ul className='w-full flex items-center justify-between text-2xl '>
+                    {
+                        navigation.map(item=>{
+                            return(
+                                <ListItem 
+                                    itemtext={item.text} 
+                                    itemClass={item.class} 
+                                    itemChildren={
+                                        
+                                    <AnchorLink 
+                                        anchorLink={item.link} 
+                                        anchorTitle={item.title} 
+                                        anchorText={item.text} 
+                                        anchorClass={'hover:font-bold w-full h-full'}
+                                    />}
+                                />                            
+                            )
+                        })
+                    }
+                </ul>
+            </nav>
+
+            <div className="flex items-center justify-end logo-container text-right w-3/12 relative">
+                <h1 className=' text-black bg-orange-300 p-4 w-max font-bold'>
+                    JM
+                </h1>
+            </div>
+
+        </header>
+    )
 }
